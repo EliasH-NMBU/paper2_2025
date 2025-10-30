4da712d2de5369edd885fa380cb90b61c581f658
Elias Evjen Hartmark <elias.hartmark@gmail.com>
2025-10-30 14:23:56 +0100

    fix numeric distinction for classes. Works with main now.

--- nuXmvHandler.py
+++ nuXmvHandler.py
@@ -5,6 +5,7 @@ import os
 
 def check_equivalence_master(formula1, formula2):
     
+    
     def normalize(f):
         return (f.replace("∧", "&")
                 .replace("∨", "|")
@@ -22,15 +23,15 @@ def check_equivalence_master(formula1, formula2):
     model = f"""
     MODULE main
     VAR
-    Alert : boolean;
-    classifier : {{none, trained, untrained}};
+    alert : boolean;
+    classifier : {{0, 1, 2}};
     dgt_3 : boolean;
     dgt_7 : boolean;
     distance_to_target : 0..10;
-    Halt : boolean;
+    halt : boolean;
     OpState : {{0, 1, 2, 3}};
-    Slowdown : boolean;
-    TurnoffUVC : boolean;
+    slowdown : boolean;
+    turnoffUVC : boolean;
 
     LTLSPEC ({f1}) <-> ({f2})
     """
@@ -64,11 +65,14 @@ def check_equivalence_master(formula1, formula2):
 if __name__ == "__main__":
 
     # Example usage
-    f1 = "H((classifier = trained & dgt_7) -> (OpState = 1))"
-    f2 = "H((classifier = trained) -> (dgt_7 -> (OpState = 1)))"
+    f1 = "H((classifier = 1 & dgt_7) -> (OpState = 1))"
+    f2 = "H((classifier = 1) -> (dgt_7 -> (OpState = 1)))"
 
-    g1 = "H((OpState=1 → (Alert ∧ ¬Slowdown ∧ ¬Halt ∧ ¬TurnoffUVC)))"
+    g1 = "H((OpState=1 → (alert ∧ ¬slowdown ∧ ¬halt ∧ ¬turnoffUVC)))"
     g2 = "(H ((OpState = 1) -> ((((! slowdown) & (! halt)) & alert) & (! turnoffUVC))))"
 
-    equiv = check_equivalence_master(g1, g2)
+    h1 = "(H ((classifier = 2) -> ((! dgt_3) -> (OpState = 3))))"
+    h2 = "H(((classifier = 2) ∧ ¬dgt_3) → (OpState = 3))"
+
+    equiv = check_equivalence_master(h1, h2)
     print("\nEquivalent:", equiv)
